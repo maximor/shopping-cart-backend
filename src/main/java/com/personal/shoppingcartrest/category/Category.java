d4bc3802463d1db37683422e9fd58a2361a9ea26
Maximo Rodriguez <maximo1225@gmail.com>
2020-11-21 16:23:14 -0400

    Category entity.

---
+++ src/main/java/com/personal/shoppingcartrest/category/Category.java
@@ -0,0 +1,104 @@
+package com.personal.shoppingcartrest.category;
+
+import com.personal.shoppingcartrest.user.User;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, unique = true)
+    private long id;
+
+    @CreatedDate
+    @CreationTimestamp
+    private Date creationDate;
+
+    @LastModifiedDate
+    @UpdateTimestamp
+    private Date updatedDate;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = true)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean active = false;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Category() {
+    }
+
+    public Category(String name, String description, User user) {
+        this.name = name;
+        this.description = description;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
