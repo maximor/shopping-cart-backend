a28a1ec4854b505275a7ed022d4d1b67d20f9f97
Maximo Rodriguez <maximo1225@gmail.com>
2020-11-20 22:00:48 -0400

    first commit

---
+++ src/main/java/com/personal/shoppingcartrest/ShoppingCartRestApplication.java
@@ -0,0 +1,13 @@
+package com.personal.shoppingcartrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCartRestApplication.class, args);
+    }
+
+}
