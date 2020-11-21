29daf301fb47eeb4d5d05d331c79aeef18ac5763
Maximo Rodriguez <maximo1225@gmail.com>
2020-11-21 18:22:33 -0400

    Order entity.

---
+++ src/main/java/com/personal/shoppingcartrest/order/Order.java
@@ -0,0 +1,90 @@
+package com.personal.shoppingcartrest.order;
+
+import com.personal.shoppingcartrest.user.User;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, unique = true)
+    private long id;
+
+    @CreatedDate
+    @CreationTimestamp
+    private Date creationDate;
+
+    @Column(name = "total_cost", nullable = false)
+    private float totalCost;
+
+    @Column(nullable = false)
+    private String status = "open";
+
+    @Column(nullable = false)
+    private boolean cancelled = false;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Order() {
+    }
+
+    public Order(float totalCost, String status, User user) {
+        this.totalCost = totalCost;
+        this.status = status;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public float getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(float totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
